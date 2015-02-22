bbef0282eda3b696bce44786507aa754acd37c22
William Wennerström <william@willeponken.me>
2015-02-22 07:56:18 +0100

    Some testing, need to check highest range in registration

--- test/client.js
+++ test/client.js
@@ -1,24 +1,30 @@
 var net = require('net');
 
-var password = 'examplePassword';
 
-var json = JSON.stringify({
-  password: password,
-  pubkey: 'testpubkey.k',
-  misc: 'Cellphone 1231231232, Email john@doe'
-});
+var cnt = 0;
+setInterval(function() {
+  var password = 'examplePassword';
 
-var client = net.connect({ port: 4132 },
-    function() { //'connect' listener
-      console.log('connected to server!');
-      client.write(json);
-    });
+  var json = JSON.stringify({
+    password: password,
+    pubkey: 'testpubkey' + cnt + '.k',
+    misc: 'Cellphone 1231231232, Email john@doe'
+  });
+  
+  var client = net.connect({ port: 4132 },
+      function() { //'connect' listener
+        console.log('connected to server!');
+        client.write(json);
+      });
 
-client.on('data', function(data) {
-  console.log(data.toString());
-  //client.end();
-});
+  client.on('data', function(data) {
+    console.log(data.toString());
+    //client.end();
+  });
 
-client.on('end', function() {
-  console.log('disconnected from server');
-});
+  client.on('end', function() {
+    console.log('disconnected from server');
+  });
+
+  cnt++;
+}, 1000);
