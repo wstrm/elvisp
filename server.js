ad2a3867ac24fcfe150f110f13d0f919dd5a08ef
William Wennerström <william@willeponken.me>
2016-05-09 16:33:14 +0200

    Added nconf as config store, updated typos

--- server.js
+++ server.js
@@ -2,42 +2,49 @@
 
 var elvisp = require('./lib/elvisp.js'),
     log  = require('./lib/log.js'),
+    nconf = require('nconf'),
     fs   = require('fs');
 
-var config = {
-  password: 'testPassword',
-  listen: 4132,
-  iptunnel: {
-    range: [
-      ['2a03', 'b0c0', '0002', '00d0', '0000', '0000', '01c0', 'f000'],
-      ['2a03', 'b0c0', '0002', '00d0', '0000', '0000', '01c0', 'f00f']
-    ],
-    prefix: 0
-  },
-  db: __dirname + '/db'
-};
+function parseConfig(config) {
 
-var cjdnsadmin = JSON.parse(fs.readFileSync(process.env.HOME + '/.cjdnsadmin'));
-var cjdroute = fs.readFileSync(cjdnsadmin.config);
+  nconf.use('memory');
+  nconf.load();
 
-try {
-  cjdroute = JSON.parse(cjdroute);
-} catch (err) {
-  log.warn('Failed to parse JSON, falling back to eval');
+  nconf.argv().env();
+    
+  nconf.file({
+    file: './config.json'
+  });
 
-  /* jshint ignore:start */
-  eval('cjdroute = ' + cjdroute); // TODO: Find way of doing this without `eval`.
-  /* jshint ignore:end */
+  nconf.defaults({
+    "password": "testPassword",
+    "listen": 4132,
+    "iptunnel": {
+      "range": [
+        ["2a03", "b0c0", "0002", "00d0", "0000", "0000", "01c0", "f000"],
+        ["2a03", "b0c0", "0002", "00d0", "0000", "0000", "01c0", "f00f"]
+      ],
+      "prefix": 0
+    },
+    "cjdns": {
+      "password": "ycdzz73bn17k22c017xtdxgmq7kn7xq",
+      "pubkey": "lpu15wrt3tb6d8vngq9yh3lr4gmnkuv0rgcd2jwl5rp5v0mhlg30.k",
+      "port": 11234,
+      "address": "127.0.0.1"
+    },
+    "db": "./db"
+  });
+
+  return nconf;
 }
 
-config.cjdns = cjdnsadmin;
-config.cjdns.pubkey = cjdroute.publicKey;
+var config = parseConfig('./config.json');
 
 var elvispServer = new elvisp(config);
 elvispServer.listen();
 
 /*
- * Reload IPTd on SIGHUP
+ * Reload Elvisp on SIGHUP
  * This is useful if cjdns has crashed or restarted
  * and you want to load all the registered users into
  * cjdns again.
