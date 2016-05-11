30f59a894377af29fddb211d349ead57e1f74023
William Wennerström <william@willeponken.me>
2016-05-11 14:51:02 +0200

    Updated protocol v1, added a prototype in go

---
+++ server/server.go
@@ -0,0 +1,91 @@
+package server
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/willeponken/elvisp/tasks"
+)
+
+func taskFactory(input string) tasks.TaskInterface {
+	t := tasks.Task{}
+
+	array := strings.Split(input, " ")
+	if len(array) >= 2 {
+		cmd := strings.ToLower(array[0])
+		argv := array[1:]
+
+		t.argv = argv
+
+		switch cmd {
+		case "register":
+			return tasks.Register{t}
+		case "remove":
+			return tasks.Remove{t}
+		case "lease":
+			return tasks.Lease{t}
+		case "release":
+			return tasks.Release{t}
+		}
+	}
+
+	return tasks.Invalid{t}
+}
+
+func taskRunner(t tasks.TaskInterface, out chan string) {
+	out <- t.run() + "\n"
+}
+
+func requestHandler(conn net.Conn, out chan string) error {
+	defer close(out)
+
+	for {
+		line, err := bufio.NewReader(conn).ReadBytes('\n')
+		if err != nil {
+			return err
+		}
+
+		t := taskFactory(strings.TrimRight(string(line), "\n"))
+		go taskRunner(t, out)
+	}
+}
+
+func sendHandler(conn net.Conn, in <-chan string) {
+	defer conn.Close()
+
+	for {
+		message := <-in
+		rb, err := io.Copy(conn, bytes.NewBufferString(message))
+
+		if rb == 0 || err != nil {
+			return
+		}
+	}
+}
+
+// Listen to the defined port.
+func Listen(port string) error {
+
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		return err
+	}
+
+	var conn net.Conn
+	for {
+		conn, err = ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		channel := make(chan string)
+
+		go requestHandler(conn, channel)
+		go sendHandler(conn, channel)
+	}
+}
---
+++ tasks/tasks.go
@@ -0,0 +1,80 @@
+package tasks
+
+const (
+	errorInvalidLength = "2 Invalid length of arguments"
+	errorInvalidTask   = "1 Invalid task specified"
+)
+
+// TaskInterface defines the methods needed for a default task
+type TaskInterface interface {
+	Run() string
+}
+
+// Task contains the arguments that the task needs
+type Task struct {
+	argv []string
+}
+
+func (t Task) errorString(e string) string {
+	return "error " + e
+}
+
+func (t Task) successString(s string) string {
+	return "success " + s
+}
+
+// Add should implement the add task
+type Add struct{ Task }
+
+// Remove should implement the remove task
+type Remove struct{ Task }
+
+// Lease should implement the lease task
+type Lease struct{ Task }
+
+// Release should implement the release task
+type Release struct{ Task }
+
+// Invalid should implement an invalid task which is returned if no task could be found for a command
+type Invalid struct{ Task }
+
+// Run adds a user using the public key and a token
+func (t Add) Run() string {
+	if len(t.argv) < 3 {
+		return t.errorString(errorInvalidLength)
+	}
+
+	return t.successString("<<LEASED ADDRESS HERE>>")
+}
+
+// Run ...
+func (t Remove) Run() string {
+	if len(t.argv) != 2 {
+		return t.errorString(errorInvalidLength)
+	}
+
+	return t.successString("Removed user: " + t.argv[0])
+}
+
+// Run ...
+func (t Lease) Run() string {
+	if len(t.argv) != 2 {
+		return t.errorString(errorInvalidLength)
+	}
+
+	return t.successString("<<LEASED ADDRESS HERE>>")
+}
+
+// Run ...
+func (t Release) Run() string {
+	if len(t.argv) != 2 {
+		return t.errorString(errorInvalidLength)
+	}
+
+	return t.successString("Released lease for user: " + t.argv[0])
+}
+
+// Run ...
+func (t Invalid) Run() string {
+	return t.errorString(errorInvalidTask)
+}
