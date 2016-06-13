781f01cc1e2ca3c2898532413af23c4047d3a597
William Wennerström <william@willeponken.me>
2016-06-13 14:01:45 +0200

    Added more tests, to see if it is able to calculate between subnet and return errors

--- lease/lease_test.go
+++ lease/lease_test.go
@@ -13,8 +13,10 @@ var generateTests = []struct {
 }{
 	{"192.168.1.0/24", 42, net.ParseIP("192.168.1.42"), false},
 	{"192.168.1.0/24", 256, net.ParseIP("192.168.2.0"), true},
+	{"12.21.0.0/16", 65536, net.ParseIP("12.22.0.0"), true},
 	{"1234::1222:0/16", 423411, net.ParseIP("1234::1228:75f3"), false},
 	{"1234::1222:0/120", 256, net.ParseIP("1234::1222:100"), true},
+	{"3214:1261:afb2::0/96", 4294967296, net.ParseIP("3214:1261:afb2::1:0:0"), true},
 }
 
 func TestGenerate(t *testing.T) {
@@ -27,6 +29,10 @@ func TestGenerate(t *testing.T) {
 
 		if err != nil && !tests.err {
 			t.Errorf("Row: %d returned unexpected error: %s", row, err.Error())
+		}
+
+		if err == nil && tests.err {
+			t.Errorf("Row: %d expected error but got %v", row, err)
 		}
 	}
 }
